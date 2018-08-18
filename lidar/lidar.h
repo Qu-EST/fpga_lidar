e0d30015fbfa1ba15f5dd1471cc43f8e770c2b89
jeeva91 <jee1199@gmail.com>
2018-08-18 10:37:33 -0400

    'starting_to_code_fpga'

---
+++ lidar/lidar.h
@@ -0,0 +1,15 @@
+
+/* mirror functions */
+int changePosition(int fd, float x, float y);
+float* getPosition(int fd);
+
+/* delay functions */
+int changeDelay(int fd, float dly);
+float  getDelay(int fd);
+
+
+
+/* create a structure for the mirror parameters */
+struct mirrorParams{
+};
+/*  */
